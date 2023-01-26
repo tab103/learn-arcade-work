d1e91ebc2a3816193488abb8f2576c545cfb6689
professorwade <schofieldw@centralvirginia.edu>
2023-01-26 09:23:51 -0500

    Example

---
+++ Scratch Work/variablesnexpressions.py
@@ -0,0 +1,45 @@
+# import ?
+
+# note behavior of interpreted language...
+x = 5
+z = 3
+print(x,z)
+
+# note repl in IDE
+
+# a variable in quotes is just a string
+print("x")
+
+# python is case-sensitive
+#print(Z)
+
+"""
+Good Python variable names:
+temperature_in_celsius
+tree_position_1
+tree_position_2
+car_speed
+number_of_children
+
+Bad Python variable names:
+player score
+3level
+"""
+
+PI = 3.14159 # good constant
+
+answerA = True
+answerB = False
+
+print(answerA or answerB)
+print(answerA and answerB)
+print(not answerA)
+
+answerC = 10
+answerD = 20
+
+print((answerC < answerD))
+
+answerC *= answerD
+answerC = answerC * answerD
+print(answerC)
